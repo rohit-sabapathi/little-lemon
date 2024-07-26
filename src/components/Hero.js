df76495aebadce256c8ba63ba6e1a070febb2bb7
Rohit Sabapathi <rohitsabapathi@gmail.com>
2024-07-26 16:48:44 +0530

    new commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,7 +6,8 @@ export default function Hero() {
         <div className='hero-text' >
             <h1>Little Lemon</h1>
             <h3>Chicago</h3>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+            <p>We are a family owned Mediterranean restaurant,</p>
+            <p> focused on traditional recipes served with a modern twist.</p>
         </div>
     </div>
   )
